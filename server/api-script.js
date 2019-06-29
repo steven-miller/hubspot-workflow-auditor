8279edbcc7a8e0e29e6fc83393a43e94abf086d1
Steve Miller <ben.el.elyon@gmail.com>
2019-06-29 11:55:30 -0400

    Successfully evaluating duplicate actions

--- server/api-script.js
+++ server/api-script.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 const request = require('request');
 const Bottleneck = require('bottleneck');
+const base32 = require('base32');
 
 // powerhouse, a true big dawg
 let db;
 let workflowsDb;
-let actionsDb;
+let stepsDb;
 
 const limiter = new Bottleneck({
   maxConcurrent: 1,
@@ -18,10 +19,12 @@ MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, (er
   db = client.db('workflows');
 
   workflowsDb = db.collection('workflows');
-  actionsDb = db.collection('actions');
+  stepsDb = db.collection('steps');
   // set external workflow ID and base32 action values to be unique
   workflowsDb.createIndex({ 'workflowId': 1 }, { 'unique': true });
-  actionsDb.createIndex({ 'actionId': 1 }, { 'unique': true });
+  workflowsDb.createIndex({ 'portalId': 1 }); // allow lookup later
+  stepsDb.createIndex({ 'actionId': 1 }, { 'unique': true });
+  stepsDb.createIndex({ 'portalId': 1 }); // allow lookup later
 })
 
 const populateDatabase = async () => {
@@ -56,7 +59,24 @@ const populateDatabase = async () => {
     if (err) { throw err; }
   });
 
+  for (let j = 0; j < activeWorkflows.length; j++) {
+    let workflow = activeWorkflows[j];
+    for (let k = 0; k < workflow.actions.length; k++) {
+      let action = workflow.actions[k];
+      console.log('evaluating action --- ', action);
+      if ((action.type === 'SET_CONTACT_PROPERTY' && action.propertyName !== '') || (action.type === 'LEAD_ASSIGNMENT' && (action.owners.length !== 0 || action.teamId !== null)) || (action.type === 'EMAIL' && action.emailContentId !== 0)) {
+        try {
+          let result = await evaluateAction(action, workflow.portalId, workflow.id);
+          console.log(result);
+        } catch (err) {
+          throw err;
+        }
+      }
+    }
+    // add each segment to the database
+    // add each action to the database
 
+  }
 };
 
 const getAllWorkflows = (hapikey) => {
@@ -145,14 +165,66 @@ const activeEvaluation = (workflow, history) => {
   })
 }
 
+const evaluateAction = (action, portalId, workflowId) => {
+  console.log('workflowId', workflowId)
+  return new Promise(async (resolve, reject) => {
+    try {
+      let actionId;
+      if (action.type === 'SET_CONTACT_PROPERTY') {
+        actionId = base32.encode(action.type + action.propertyName + action.newValue + portalId);
+      } else if (action.type === 'LEAD_ASSIGNMENT') {
+        actionId = base32.encode(action.type + action.ownerProperty + JSON.stringify(action.owners) + JSON.stringify(action.teamId) + portalId);
+      } else if (action.type === 'EMAIL') {
+        actionId = base32.encode(action.type + action.emailContentId + portalId);
+      }
+      console.log('base32 action id', actionId)
+      let result = await upsertAction(action, 'action', actionId, portalId, workflowId);
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+const evaluateSegment = (segment) => {
+
+};
+
+const upsertAction = (action, category, actionId, portalId, workflowId) => {
+  return new Promise(async (resolve, reject) => {
+    console.log('actionId', actionId);
+    stepsDb.findOne({ actionId: actionId }, (err, document) => {
+      console.log(document);
+      if (err) {
+        reject(err);
+      }
+      if (document === null) {
+        // mutating object - would be nice to find alternative
+        action.actionId = actionId;
+        action.portalId = portalId;
+        action.workflowId = [workflowId];
+        action.workflowCount = 1;
+        action.category = category;
+        stepsDb.insert(action, (err, records) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(records);
+          }
+        });
+      } else {
+        document.workflowId.push(workflowId);
+        document.workflowCount++;
+        stepsDb.updateOne({ _id: document._id }, { $set: document }, (err, records) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(records);
+          }
+        });
+      }
+    })
+  });
+};
+
 populateDatabase();
-
-
-// get all workflows
-  // iterate through ids
-    // call 1 - wf data
-    // call 2 - engagement info, label recentEnrollments = false
-    // if engagement info null && no updates L30 || enabled == false && no updates L30
-      // label "archive: true";
-    // else archive: false
-  // insert into mongo
